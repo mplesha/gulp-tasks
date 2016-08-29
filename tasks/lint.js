72bbc657a80222d9f7c7e32f3a2e8e5237b47103
mplesha <tes.t@ukr.net>
2016-08-29 18:40:03 +0300

    Update lint.js

--- tasks/lint.js
+++ tasks/lint.js
@@ -4,7 +4,7 @@ const gulp = require('gulp');
 const eslint = require('gulp-eslint');
 
 gulp.task('lint', () => {
-    gulp.src(['**/*.js', '!node_modules/**', '!out/**', '!main/public/**'])
+    gulp.src(['**/*.js', '!node_modules/**', '!main/public/**'])
         .pipe(eslint())
         .pipe(eslint.format())
         .pipe(eslint.failAfterError());
